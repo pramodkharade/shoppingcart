8bf989ee4f83d0af06c95b3b42df2f27f760d4af
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-02 15:32:34 +0530

    created separate routes files

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/',(req,res,next)=>{
+router.get('/',(req,res,next)=>{
     res.send('<h1>Hello Express Application</h1>')
 });
 
