4bfa4292234c9182e16bf933110395bea69fb4da
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-02 15:17:49 +0530

    added router by use method and post method

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port  = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="post"><input name="title"><button type="submit">add</button></form');
+});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.send('<h1>Product Page</h1>');
+});
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello Express Application</h1>')
+});
 app.listen(port,()=>{
     console.log('Server is running on ',port);
 });
