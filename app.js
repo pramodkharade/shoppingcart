75e57b937249f24224aed9f15d9cd01207ebbd62
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-02 15:31:42 +0530

    imported the routes files in app to use

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const app = express();
 const port  = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="post"><input name="title"><button type="submit">add</button></form');
-});
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.send('<h1>Product Page</h1>');
-});
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello Express Application</h1>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 app.listen(port,()=>{
     console.log('Server is running on ',port);
 });
