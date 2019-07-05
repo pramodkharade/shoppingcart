6c8d14d5938784ca6e228ead621a0d3538a62a98
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-05 17:38:49 +0530

    file product data in json file

--- controllers/products.js
+++ controllers/products.js
@@ -10,6 +10,8 @@ exports.getAddProducts = (req,res,next)=>{
 };
 
 exports.getProducts = (req,res,next)=>{
-    const products = Product.fetchAll();
-    res.render('shop',{prods:products,pageTitle:'Shop',path:'/'});
+    Product.fetchAll((products)=>{
+        res.render('shop',{prods:products,pageTitle:'Shop',path:'/'});
+    });
+    
 };
--- models/product.js
+++ models/product.js
@@ -1,12 +1,31 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const filepath = path.join(__dirname,
+    '../data',
+    'products.json');
+const getProductsFromFile = (cb)=>{
+    
+        fs.readFile(filepath,(err,data)=>{
+            if(err){
+                cb([]);
+            }else{
+                cb(JSON.parse(data));
+            }
+        });
+}
 module.exports = class Product {
     constructor(title){
         this.title = title;
     }
     save(){
-        products.push(this);
+        getProductsFromFile((products)=>{
+            products.push(this);
+            fs.writeFile(filepath,JSON.stringify(products),(error)=>{
+                console.log(error);
+            });
+        });
     }
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        getProductsFromFile(cb);
     }
 }
