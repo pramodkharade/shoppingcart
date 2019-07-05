34a2a6f48493dda9ccd68c4bac6555212da67554
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-05 16:02:34 +0530

    add the product by model class

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,15 @@
-const products=[];
+const Product = require('../models/product');
 exports.getAddProducts = (req,res,next)=>{
     res.render('add-product',{pageTitle:'Add Product',path:'/admin/add-product'});
  };
 
  exports.postAddProducts = (req,res,next)=>{
-    products.push({title:req.body.title});
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/')
 };
 
 exports.getProducts = (req,res,next)=>{
+    const products = Product.fetchAll();
     res.render('shop',{prods:products,pageTitle:'Shop',path:'/'});
 };
---
+++ models/product.js
@@ -0,0 +1,12 @@
+const products = [];
+module.exports = class Product {
+    constructor(title){
+        this.title = title;
+    }
+    save(){
+        products.push(this);
+    }
+    static fetchAll(){
+        return products;
+    }
+}
