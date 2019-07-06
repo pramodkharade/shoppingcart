8f44aa1a1fdfaa59ee237abd27ff14c25070f41c
pramodkharade <kharade.pramod91@gmail.com>
2019-07-06 15:34:40 +0530

    rendering product view details

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,9 @@ exports.getProducts = (req,res,next)=>{
 exports.getProduct = (req,res,next)=>{
     const prodId = req.params.productId;
     console.log(Product.findById(prodId,(product)=>{
-        console.log(product);
+        res.render('shop/product-details',{product:product,pageTitle:product.title,path:'/products'})
     }));
-    res.redirect('/');
+    
 }
 
 exports.getIndex = (req,res,next)=>{
