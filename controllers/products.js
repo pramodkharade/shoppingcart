02507282fe260c2a148e47fd580c8186bd9a42b3
pramodkharade <kharade.pramod91@gmail.com>
2019-07-06 11:40:21 +0530

    added new folder structure for shopping admin and customer facing

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 exports.getAddProducts = (req,res,next)=>{
-    res.render('add-product',{pageTitle:'Add Product',path:'/admin/add-product'});
+    res.render('admin/add-product',{pageTitle:'Add Product',path:'/admin/add-product'});
  };
 
  exports.postAddProducts = (req,res,next)=>{
@@ -11,7 +11,7 @@ exports.getAddProducts = (req,res,next)=>{
 
 exports.getProducts = (req,res,next)=>{
     Product.fetchAll((products)=>{
-        res.render('shop',{prods:products,pageTitle:'Shop',path:'/'});
+        res.render('shop/product-list',{prods:products,pageTitle:'Shop',path:'/'});
     });
     
 };
