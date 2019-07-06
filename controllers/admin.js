c15bb6c347d80468bb169a320606864ac49ec0e2
pramodkharade <kharade.pramod91@gmail.com>
2019-07-06 13:35:19 +0530

    added product form with new field and saved data in file and css

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,11 @@ exports.getAddProducts = (req,res,next)=>{
  };
 
  exports.postAddProducts = (req,res,next)=>{
-    const product = new Product(req.body.title);
+   const title = req.body.title;
+   const imageUrl = req.body.imageUrl;
+   const price = req.body.price;
+   const description = req.body.description;
+    const product = new Product(title,imageUrl,price,description);
     product.save();
     res.redirect('/')
 };
--- models/product.js
+++ models/product.js
@@ -14,8 +14,11 @@ const getProductsFromFile = (cb)=>{
         });
 }
 module.exports = class Product {
-    constructor(title){
+    constructor(title,imageUrl,price,description){
         this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
     save(){
         getProductsFromFile((products)=>{
