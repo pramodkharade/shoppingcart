b316d942ea18c7b5d4f4891020abe5ef2aedab51
pramodkharade <kharade.pramod91@gmail.com>
2019-07-06 14:57:03 +0530

    extracting the dynamic params

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,11 @@ exports.getProducts = (req,res,next)=>{
                     });
     });
 };
+exports.getProduct = (req,res,next)=>{
+    const prodId = req.params.productId;
+    console.log(prodId);
+    res.redirect('/');
+}
 
 exports.getIndex = (req,res,next)=>{
     Product.fetchAll((products)=>{
--- routes/shop.js
+++ routes/shop.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/',shopController.getIndex);
 router.get('/products',shopController.getProducts);
+router.get('/products/:productId',shopController.getProduct);
 router.get('/cart',shopController.getCart);
 router.get('/orders',shopController.getOrders);
 router.get('/checkout',shopController.getCheckout);
