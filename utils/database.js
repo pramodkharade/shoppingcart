7d491af12be910993bd7d797099305e2bbef62ed
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-09 17:14:26 +0530

    create connection with mysql using sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,7 @@
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'node_db_shoppingcart'
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node_db_shoppingcart','root','root',{
+    dialect:'mysql',
+    host:'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
