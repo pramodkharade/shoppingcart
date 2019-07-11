b49c4340797442499a65e9a86ef760907de424bf
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-11 15:37:19 +0530

    Getting a Single Product with the

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,16 +18,15 @@ exports.getProducts = (req, res, next) => {
 };
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    Product.findById(prodId)
-           .then(([product])=>{
-               console.log("Product of:",product[0]);
-            res.render('shop/product-details',
-            {  product: product[0],
-               pageTitle: product.title,
-               path: '/products' 
-           })
-           })
-           .catch(()=>{});
+    Product.findByPk(prodId)
+    .then(product => {
+      res.render('shop/product-details', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
 
 }
 
