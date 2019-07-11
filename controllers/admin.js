ce73f6afdef1270dff187e1f32bf4bf4b46d20d2
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-11 17:09:19 +0530

    delete the product by using sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,6 +22,7 @@ exports.postAddProducts = (req, res, next) => {
   })
   .then((result)=>{
     console.log("Created the product");
+    res.redirect('/admin/products');
   })
   .catch((error)=>{
     console.log("Error Insert:",error);
@@ -92,6 +93,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req,res,next)=>{
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  // Product.deleteById(prodId);
+  Product.findByPk(prodId)
+  .then((product)=>{
+      return product.destroy();
+  }).then((result)=>{
+    console.log('Product deleted');
+    res.redirect('/admin/products');
+  })
+  .catch((error)=>{
+      console.log(error);
+  });
 }
