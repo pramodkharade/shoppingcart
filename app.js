474a740748c39b821051f5e3d1846bf2a2c27cc1
pramod.kharade <pramod.kharade@blazeclan.com>
2019-07-11 11:46:13 +0530

    Syncing JS Definitions to the

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ const db = require('./utils/database');
 const rootDir = require('./utils/path');
 const adminRouter = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const sequelize = require('./utils/database');
 const app = express();
 //app.engine('hbs',expressHbs());
 app.set('view engine','ejs');
  //app.set('view engine','pug');
 app.set('views','views');
 const port  = process.env.PORT || 4000;
-db.execute('select * from products')
-    .then((result)=>{
-        console.log(result[0]);
-    })
-    .catch((error)=>{
-        console.log(error);
-    });
+// db.execute('select * from products')
+//     .then((result)=>{
+//         console.log(result[0]);
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     });
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRouter.router);
@@ -26,6 +27,13 @@ app.use((req,res,next)=>{
     //res.status(404).sendFile(path.join(rootDir,'views','404.html'));
     res.status(404).render('404',{pageTitle:'Page Not found',path:'/'})
 });
+sequelize.sync()
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((error)=>{
+        console.log('Sync:',error);
+    });
 app.listen(port,()=>{
     console.log('Server is running on ',port);
 });
