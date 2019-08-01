968316e14532796ff32d50189588baa307906c55
pramod.kharade <pramod.kharade@blazeclan.com>
2019-08-01 09:56:09 +0530

    fetch all products using mongooose

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
 
-    Product.fetchAll()
+    Product.find()
         .then((products) => {
             res.render('shop/product-list',
                 {
@@ -31,7 +31,7 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then(products => {
             res.render('shop/index', {
                 prods: products,
