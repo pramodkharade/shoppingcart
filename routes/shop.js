5075bbb60368ec19adc696dda5fd12c0c69ff404
pramod.kharade <pramod.kharade@blazeclan.com>
2019-08-01 13:58:35 +0530

    addto cart using mongoose

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/',shopController.getIndex);
 router.get('/products',shopController.getProducts);
 router.get('/products/:productId',shopController.getProduct);
-router.get('/cart',shopController.getCart);
+// router.get('/cart',shopController.getCart);
 // router.post('/cart',shopController.postCart);
 router.post('/cart',shopController.postCart);
 router.post('/cart-delete-item',shopController.postCartDeleteProduct);
