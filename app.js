dd735dd43e9c97e001a17756f27aa91059f70dea
pramodkharade <kharade.pramod91@gmail.com>
2019-08-02 17:06:46 +0530

    store the session in mongodb using mongodbStore library

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const session = require('express-session');
+const mongodbStore = require('connect-mongodb-session')(session);
 
 const adminRouter = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 const User = require('./models/user');
-
+const MONGOURI ='mongodb://127.0.0.1:27017/shoppingCart';
 const app = express();
+const store = new mongodbStore({
+    uri: MONGOURI,
+    collection:'sessions'
+});
 //app.engine('hbs',expressHbs());
 app.set('view engine', 'ejs');
 //app.set('view engine','pug');
@@ -18,7 +23,12 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:'thisisthenodeapplication', resave:false,saveUninitialized:false}));
+app.use(session({
+    secret:'thisisthenodeapplication',
+     resave:false,
+     saveUninitialized:false,
+    store:store
+    }));
 app.use((req, res, next) => {
     User.findById('5d43e9bce57b8c22a009ee41')
         .then((user) => {
@@ -37,7 +47,7 @@ app.use((req, res, next) => {
     res.status(404).render('404', { pageTitle: 'Page Not found', path: '/' })
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/shoppingCart', { useNewUrlParser: true })
+mongoose.connect(MONGOURI, { useNewUrlParser: true })
     .then((result) => {
         User.findOne().then(user => {
             if (!user) {
