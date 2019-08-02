e61c4715ba3e8201961a90c725a6b2007c2947fd
pramodkharade <kharade.pramod91@gmail.com>
2019-08-02 16:46:41 +0530

    store the session and send to request

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,15 @@
 exports.getlogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').split('=')[1].trim()==='true';
+    //const isLoggedIn = req.get('Cookie').split('=')[1].trim()==='true';
     
-    console.log('Cookie is:', isLoggedIn);
+    //console.log('Cookie is:', isLoggedIn);
     res.render('auth/login', {
         path: "/login",
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });
 };
 
 exports.postlogin = (req, res, next) => {
-    res.setHeader('Set-Cookie','isLoggedIn=true; Max-age=10');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 };
