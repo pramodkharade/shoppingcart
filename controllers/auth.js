0b0d27c1cef7afb06b6f1dbff17044f0d2fb91c0
pramod.kharade <pramod.kharade@blazeclan.com>
2019-08-05 11:52:01 +0530

    add custom error message

--- controllers/auth.js
+++ controllers/auth.js
@@ -91,7 +91,7 @@ exports.postSignup = (req, res, next) => {
       path: '/signup',
       pageTitle: 'Signup',
       isAuthenticated: false,
-      errorMessage: errors.array()
+      errorMessage: errors.array()[0].msg
     });
   }
   User.findOne({
--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.post('/login',authController.postlogin);
 router.get('/signup', authController.getSignup);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email'), authController.postSignup);
 router.post('/logout',authController.postlogout);
 router.get('/reset/:token',authController.getNewPassword);
 router.post('/new-password',authController.postNewPassword);
